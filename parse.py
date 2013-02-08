edb554ef2109c28f57bdfb8220233b95c18784e3
Sebastian J. Mielke <sjm1835@gmail.com>
2013-02-08 18:19:53 +0100

    Python baut nun Dictionaries aus den Ratings- und Genre-Listen. Zeit für Datenbankzeug.

--- parse.py
+++ parse.py
@@ -1,12 +1,30 @@
 #! /usr/bin/python
+ratingsfile = "/home/sjm/downloads/ratings.list"
+genresfile = "/home/sjm/downloads/genres.list"
 
 toplist = []
 
-with open("/home/sjm/downloads/ratings.list") as f:
+with open(ratingsfile) as f:
 	for i in range(28): f.readline()
-	for i in range(250):
+	for i in range(10):
 		toplist.append(f.readline())
 
-for line in toplist:
-	line = line[32:-1]
-	print line
+for i in range(len(toplist)):
+	toplist[i] = toplist[i][32:-1]
+
+topdict = {}
+
+with open(genresfile) as f:
+	for filmtitle in toplist:
+		genrelist = []
+		f.seek(14000) #skip intro with wrong schindlers list
+		while 1:
+			line = f.readline()
+			if line == "":
+				break
+
+			if line.find(filmtitle) >= 0:
+				genrelist.append(line[line.rfind('\t')+1:-1])
+		topdict.update({filmtitle: genrelist})
+
+print topdict
