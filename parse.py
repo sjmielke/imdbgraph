7b4b1e1b1b3ebb8cc18ac821066d31db2c93cbc9
Sebastian J. Mielke <sjm1835@gmail.com>
2013-02-09 19:58:39 +0100

    How much faster is it? It's over 9000\!

--- parse.py
+++ parse.py
@@ -1,28 +1,35 @@
 #! /usr/bin/python
-ratingsfile = "/home/sjm/downloads/ratings.list"
-genresfile = "/home/sjm/downloads/genres.list"
+ratingsfile = "ratings.list"
+genresfile = "genres.list"
 
 toplist = []
 with open(ratingsfile) as f:
 	for i in range(28): f.readline() # real list starts late in the file
-	for i in range(50):
+	for i in range(250):
 		toplist.append(f.readline())
 
 for i in range(len(toplist)):
 	toplist[i] = toplist[i][32:-1]
 
+toplist.sort()
+
+line = "blubb"
 topdict = {}
 with open(genresfile) as f:
+	f.seek(14000) # skip intro with wrong schindlers list
 	for filmtitle in toplist:
+		found = 0
 		genrelist = []
-		f.seek(14000) # skip intro with wrong schindlers list
 		while 1:
+			if line.find(filmtitle) >= 0:
+				genrelist.append(line[line.rfind('\t')+1:-1])
+				found = 1
+			elif found == 1:
+				break
+
 			line = f.readline()
 			if line == "":
 				break
-
-			if line.find(filmtitle) >= 0:
-				genrelist.append(line[line.rfind('\t')+1:-1])
 		topdict.update({filmtitle: genrelist})
 
 print topdict
